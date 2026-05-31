d92c54cba112bd3ec7a6a7923c5e44b04467e237
sans957469 <3564422187@qq.com>
2026-05-31 19:13:28 +0800

    handle OpenMV track command in vision module

--- Core/Src/vision.c
+++ Core/Src/vision.c
@@ -1,7 +1,7 @@
-#include"remote_ctrl.h"
-#include"uart_drv.h"
-#include"usart.h"
-#include"global_def.h"
+#include "remote_ctrl.h"
+#include "uart_drv.h"
+#include "usart.h"
+#include "global_def.h"
 
 #define HEADER     0xAA
 #define END        0x55
@@ -13,25 +13,39 @@ static UartDma_Rx  g_vision_uart;
 
 static void vision_on_frame(uint8_t *data, uint16_t len)
 {
-	/* TRACK: HEADER | TYPE | X | Y | CHK | END  (6 bytes) */
-	if (len != 6)
-		return;
-	if (data[0] != HEADER || data[5] != END)
-		return;
-	if (data[1] != TYPE_TRACK)
-		return;
+    /*
+     * OpenMV track packet:
+     * HEADER | TYPE | X | Y | CHK | END
+     * 0xAA   | 0x01 | X | Y | CHK | 0x55
+     *
+     * X/Y are int8 command values.
+     * CHK = TYPE ^ X ^ Y
+     */
+    for (uint16_t i = 0; i + 5 < len; i++)
+    {
+        if (data[i] != HEADER)
+            continue;
 
-	uint8_t chk = TYPE_TRACK ^ data[2] ^ data[3];
-	if (data[4] != chk)
-		return;
+        if (data[i + 1] != TYPE_TRACK)
+            continue;
 
-	int8_t x = (int8_t)data[2];
-	g_vision_cmd.turn = (int32_t)x * RC_SPEED_SCALE;
+        if (data[i + 5] != END)
+            continue;
+
+        uint8_t chk = TYPE_TRACK ^ data[i + 2] ^ data[i + 3];
+        if (data[i + 4] != chk)
+            continue;
+
+        int8_t x = (int8_t)data[i + 2];
+
+        g_vision_cmd.turn = (int32_t)x * RC_SPEED_SCALE;
+        return;
+    }
 }
 
 void Vision_Init(void)
 {
-	UartDma_Init(&g_vision_uart, &huart6,
-	             dma_buf, frame_buf, VISION_BUF_SIZE,
-	             vision_on_frame);
+    UartDma_Init(&g_vision_uart, &huart6,
+                 dma_buf, frame_buf, VISION_BUF_SIZE,
+                 vision_on_frame);
 }
